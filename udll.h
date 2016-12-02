56d2c593ddbe80f88ed3279ec88b530aec053f3e
Ryan Kartavicius <14rak2@queensu.ca>
2016-12-02 17:08:50 -0600

    Function Declarations

---
+++ udll.h
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+ 
+//function to insert data at a specific index of a doubly linked list
+void insert(int index, union Data data);
+
+//function to delete data at a specific index of a doubly linked list
+void remove(int index);
+
+//function to return data from a doubly linked list at a specific index
+union Data get(int index);
+
+//function to return the length of doubly linked list
+int length();
